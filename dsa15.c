bcc45675894cc2b22349247465f4fd2b1ab25df8
Sarveshwari Rathore <sarveshwarii8686@gmail.com>
2026-02-15 22:19:43 +0530

    Add files via upload

---
+++ dsa15.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n,sum=0;
+    scanf("%d",&n);
+    int matrix[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                sum += matrix[i][j];
+            }
+        }
+    }
+    printf("%d",sum);
+}
